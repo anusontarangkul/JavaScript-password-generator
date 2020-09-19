ece5889f02bb4c027c0a8d6e7a9c2bec74b9afcd
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-18 20:23:48 -0700

    add pseudo code

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// When clicked on password, prompts open
+
+// Prompt "What is the minimum amount of characters"
+
+// Prompt "What is the maximum amount of characters"
+
+// Prompt "What characte types to include in the password"
+// Choose lowercase, uppercase, numeric, and/or special characters
+
+// Validate input
+
+// Password is generated
+
+// Password is displayed
